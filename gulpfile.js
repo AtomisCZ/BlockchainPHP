faf5b3745dbe8c90bec61611fa3df6fa2eb129da
Filip Matiaska <matiaskafi@gmail.com>
2017-02-12 19:26:44 +0100

    Installed @aquafadas/gulp-php-minify, configured in gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+var gulp = require('gulp');
+var phpMinify = require('@aquafadas/gulp-php-minify');
+
+gulp.task('serve', function() {
+	gulp.src('app/*.php', {read: false})
+		.pipe(phpMinify({binary: 'C:\\wamp64\\bin\\php\\php7.0.10\\php.exe'}))
+		.pipe(gulp.dest('dist'));
+});
