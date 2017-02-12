d02d7609ab95be85f377618664c97a051851dd46
Filip Matiaska <matiaskafi@gmail.com>
2017-02-12 19:33:30 +0100

    Renamed BitcoinException to BlockchainException, small edit of error message

--- app/blockchain.php
+++ app/blockchain.php
@@ -1,11 +1,11 @@
 <?php
 defined('IN_INDEX') or die('<span class="error">You cannot access this file directly!</span>');
 
-class BitcoinException extends Exception {
+class BlockchainException extends Exception {
 	protected $message;
 
 	public function errorMessage() {
-		$errorMsg = '<span class="error">The bitcoin API function is not working now, please try it again later.</span><br /><span class="errorMsg">Error message: ' . $this->message . '</span>';
+		$errorMsg = '<span class="error">The blockchain.info framework is not working now, please try it again later.</span><br /><span class="errorMsg">Error message: ' . $this->message . '</span>';
 		return $errorMsg;
 	}
 
